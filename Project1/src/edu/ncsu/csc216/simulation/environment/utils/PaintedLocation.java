eafa94ba62c5d3ec8e0c4d197f066c2238015b4b
Scott Spencer <wsspence@ncsu.edu>
2016-10-05 16:52:05 -0400

    changed PaintedLocation to extend Location

--- Project1/src/edu/ncsu/csc216/simulation/environment/utils/PaintedLocation.java
+++ Project1/src/edu/ncsu/csc216/simulation/environment/utils/PaintedLocation.java
@@ -2,14 +2,14 @@ package edu.ncsu.csc216.simulation.environment.utils;
 
 import java.awt.Color;
 
-public class PaintedLocation {
+public class PaintedLocation extends Location {
 	
 	private Color tint;
 	
 	private char symbol;
 	
 	public PaintedLocation(int row, int column, Color tint, char symbol) {
-		
+		super(0,0);
 	}
 	
 	public Color getColor() {
