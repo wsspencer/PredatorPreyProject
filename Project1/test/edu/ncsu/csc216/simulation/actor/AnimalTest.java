8306f2867a08f95f3a8c42ca5da9046f9e3536c3
Scott Spencer <wsspence@ncsu.edu>
2016-10-13 10:29:21 -0400

    implemented first test.  starting to add functionality

--- Project1/test/edu/ncsu/csc216/simulation/actor/AnimalTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/actor/AnimalTest.java
@@ -7,6 +7,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.ncsu.csc216.simulation.environment.EcoGrid;
+import edu.ncsu.csc216.simulation.environment.Ecosystem;
+import edu.ncsu.csc216.simulation.environment.utils.Location;
+
 /**
  * @author wspencer
  *
@@ -14,8 +18,12 @@ import org.junit.Test;
 public class AnimalTest {
 
 	@Test
-	public void testMove() {
-		fail("Not yet implemented");
+	public void testBreed() {
+		EcoGrid grid = new Ecosystem(4, 4);
+		Location location = new Location(3, 3);
+		Animal lion = new PurePredator('L');
+		
+		assertFalse(lion.breed(location, grid));
 	}
 
 }
