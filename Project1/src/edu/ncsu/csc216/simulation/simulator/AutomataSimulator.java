6b0c54ea143280ba2d2098bae573f3d3288f95fa
Scott Spencer <wsspence@ncsu.edu>
2016-10-14 16:03:28 -0400

    trying to implement further I/O functionality

--- Project1/src/edu/ncsu/csc216/simulation/simulator/AutomataSimulator.java
+++ Project1/src/edu/ncsu/csc216/simulation/simulator/AutomataSimulator.java
@@ -5,16 +5,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import edu.ncsu.csc216.simulation.actor.Animal;
 import edu.ncsu.csc216.simulation.actor.Configs;
 import edu.ncsu.csc216.simulation.environment.EcoGrid;
 import edu.ncsu.csc216.simulation.environment.Ecosystem;
+import edu.ncsu.csc216.simulation.environment.utils.Location;
 import edu.ncsu.csc216.simulation.environment.utils.PaintedLocation;
 
 public class AutomataSimulator implements SimulatorInterface {
 	
-	private static final int SIZE = 0;
+	private static final int SIZE = 20;
 	
-	private static final int THRESHOLD = 0;
+	private static final int THRESHOLD = 20;
 	
 	private static final String SIZE_ERROR_MESSAGE = "";
 	
@@ -63,24 +65,27 @@ public class AutomataSimulator implements SimulatorInterface {
 			}
 			
 			//runs loop while file has another line, and reads in the given ecosystem as it is written
+			char[][] map = new char[20][20];
 			while (filereader.hasNextLine()) {
 				line = filereader.nextLine();
+				//divide by 20 for the column because it will return a whole integer and it's a 20x20 grid
 				for (int j = 0; j < line.length(); j++) {
-					line.charAt(j);
+					map[j][j / 20] = line.charAt(j);
 				}
 			}
 			filereader.close();
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException();
 		}
+		///do something with map created above
 		simpleSystem = new Ecosystem(SIZE, THRESHOLD);
+		
 	}
 	
 	public AutomataSimulator(String initFileName, String configFileName) {
 		this(initFileName);
 		try {
 			Scanner configReader = new Scanner(new File(configFileName));
-			String line;
 			Color[] colors = new Color[3];
 			int[] starve = new int[3];
 			int[] breed = new int[3];
@@ -133,7 +138,15 @@ public class AutomataSimulator implements SimulatorInterface {
 	}
 
 	public void step() {
-		
+		//make a animal array out of simpleSystem and enable each of its members
+		Animal[][] creature = simpleSystem.getMap();
+		for (int i = 0; i < creature.length; i++) {
+			for (int j = 0; j < creature.length; j++) {
+				if (creature[i][j] != null) {
+					creature[i][j].enable();
+				}
+			}
+		}
 	}
 
 	public PaintedLocation[][] getView() {
