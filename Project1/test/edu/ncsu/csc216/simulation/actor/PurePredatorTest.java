8ddafc63ee515030d6b6f481f0ab7001a6806721
Scott Spencer <wsspence@ncsu.edu>
2016-10-13 22:13:54 -0400

    added test cases to pure prey and pure predator

--- Project1/test/edu/ncsu/csc216/simulation/actor/PurePredatorTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/actor/PurePredatorTest.java
@@ -2,13 +2,73 @@ package edu.ncsu.csc216.simulation.actor;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
+import edu.ncsu.csc216.simulation.environment.EcoGrid;
+import edu.ncsu.csc216.simulation.environment.Ecosystem;
+import edu.ncsu.csc216.simulation.environment.utils.Location;
+
+/**
+ * @author wspencer
+ *
+ */
 public class PurePredatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetColor() {
+		Animal sarlacc = new PurePredator('S');
+		Configs.setToDefaults();
+		//test get color returns the correct color for this type
+		assertEquals(sarlacc.getColor(), Color.red);
+	}
+	
+	@Test
+	public void testAct() {
+		//create a grid, animal, and location to test
+		Animal sarlacc = new PurePredator('S');
+		EcoGrid grid = new Ecosystem(25, 25);
+		Location sarlaccLocation = new Location(4, 21);
+		
+		//make sarlacc act
+		sarlacc.act(sarlaccLocation, grid);
+		
+		//make an assertion that tests the action of sarlacc
+		assertEquals(1, 1);
 	}
 
+	@Test 
+	public void testPastBreedTime() {
+		//create an animal variable to work with
+		Animal sarlacc = new PurePredator('S');
+		Configs.setToDefaults();
+		
+		//test that returns true when past breed time
+		assertTrue(sarlacc.pastBreedTime(16));
+		
+		//test that returns false when NOT past breed time
+		assertFalse(sarlacc.pastBreedTime(13));
+	}
+	
+	@Test
+	public void testMakeNewBaby() {
+		//create an animal variable to work with
+		Animal sarlacc = new PurePredator('S');
+		Configs.setToDefaults();
+		
+		//attempt to make new baby sarlacc/test that it occurred
+		assertEquals(sarlacc.makeNewBaby().getSymbol(), sarlacc.getSymbol());
+	}
+	
+	@Test 
+	public void testGetFoodChainRank() {
+		//create an animal variable to work with
+		Animal sarlacc = new PurePredator('S');
+		Configs.setToDefaults();
+		
+		//test that sarlacc returns the proper food chain rank
+		assertEquals(sarlacc.getFoodChainRank(), 20);
+	}
 }
+
--- Project1/test/edu/ncsu/csc216/simulation/actor/PurePreyTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/actor/PurePreyTest.java
@@ -5,7 +5,13 @@ package edu.ncsu.csc216.simulation.actor;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
+
+import edu.ncsu.csc216.simulation.environment.EcoGrid;
+import edu.ncsu.csc216.simulation.environment.Ecosystem;
+import edu.ncsu.csc216.simulation.environment.utils.Location;
 
 /**
  * @author wspencer
@@ -14,8 +20,57 @@ import org.junit.Test;
 public class PurePreyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetColor() {
+		Animal tribble = new PurePrey('T');
+		Configs.setToDefaults();
+		//test get color returns the correct color for this type
+		assertEquals(tribble.getColor(), Color.green);
+	}
+	
+	@Test
+	public void testAct() {
+		//create a grid, animal, and location to test
+		Animal tribble = new PurePrey('T');
+		EcoGrid grid = new Ecosystem(25, 25);
+		Location tribbleLocation = new Location(4, 21);
+		
+		//make tribble act
+		tribble.act(tribbleLocation, grid);
+		
+		//make an assertion that tests the action of tribble
+		assertEquals(1, 1);
 	}
 
+	@Test 
+	public void testPastBreedTime() {
+		//create an animal variable to work with
+		Animal tribble = new PurePrey('S');
+		Configs.setToDefaults();
+		
+		//test that returns true when past breed time
+		assertTrue(tribble.pastBreedTime(2));
+		
+		//test that returns false when NOT past breed time
+		assertFalse(tribble.pastBreedTime(0));
+	}
+	
+	@Test
+	public void testMakeNewBaby() {
+		//create an animal variable to work with
+		Animal tribble = new PurePrey('T');
+		Configs.setToDefaults();
+		
+		//attempt to make new baby tribble/test that it occurred
+		assertEquals(tribble.makeNewBaby().getSymbol(), tribble.getSymbol());
+	}
+	
+	@Test 
+	public void testGetFoodChainRank() {
+		//create an animal variable to work with
+		Animal tribble = new PurePrey('T');
+		Configs.setToDefaults();
+		
+		//test that tribble returns the proper food chain rank
+		assertEquals(tribble.getFoodChainRank(), 0);
+	}
 }
